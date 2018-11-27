86559ff8139d9aadcae5ddd47a55f67a5224aa5d
Jatsko Polina <jatsko.polina@gmail.com>
2018-11-27 10:41:12 +0300

    Initial 'Hello world' commit

---
+++ src/main/kotlin/HelloAction.kt
@@ -0,0 +1,10 @@
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+
+class HelloAction : AnAction("Hello") {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project: Project? = event.getProject();
+        Messages.showMessageDialog(project, "Hello world!", "Greetings", Messages.getInformationIcon())
+    }
+}
