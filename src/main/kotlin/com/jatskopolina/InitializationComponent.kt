07dd75d8239f19f0453ecd03945cc1fe85fd43fe
Jatsko Polina <jatsko.polina@gmail.com>
2019-01-15 23:49:43 +0300

    On project open hook added

---
+++ src/main/kotlin/com/jatskopolina/InitializationComponent.kt
@@ -0,0 +1,17 @@
+package com.jatskopolina
+
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.project.Project
+
+class InitializationComponent (project: Project) : ProjectComponent {
+    val myProject: Project = project
+
+    override fun projectOpened() {
+        val notification: Notification = Notification("ProjectOpenNotification", "Project Opened Detected",
+            String.format("You just opened %s", myProject.name), NotificationType.INFORMATION)
+        notification.notify(myProject)
+    }
+
+}
