d262b9df62b24f8c0bf29cdec995c1f6ce70e382
Jatsko Polina <jatsko.polina@gmail.com>
2019-01-16 12:00:03 +0300

    Saving data to the configuration file

--- src/main/kotlin/com/jatskopolina/MyToolWindow.kt
+++ src/main/kotlin/com/jatskopolina/MyToolWindow.kt
@@ -5,6 +5,7 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.wm.ToolWindow
 
 import javax.swing.*
+import java.io.File
 
 class MyToolWindow(private val toolWindow: ToolWindow) {
     private var saveToolWindowButton: JButton? = null
@@ -16,23 +17,22 @@ class MyToolWindow(private val toolWindow: ToolWindow) {
         saveToolWindowButton!!.addActionListener { saveTime() }
     }
 
-
     private fun saveTime() {
         val notification: Notification?
         val time: String = inputTimeTextField!!.text
+        val message: String = inputTextTextField!!.text
         if ("^[0-9]{2}:[0-9]{2}$".toRegex().matches(time) && isFormattedTimeCorrect(time)) {
             notification = Notification(
                 "Time notification saver",
                 "Time saved successfully",
                 String.format(
                     "You wanna save the notification '%s' to show at %s",
-                    inputTextTextField!!.text,
+                    message,
                     time
                 ),
                 NotificationType.INFORMATION
             )
-            // TODO save input values
-
+            saveConfiguration(time, message)
             /*
             TODO use delayed task
             docs: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ScheduledExecutorService.html#scheduleAtFixedRate(java.lang.Runnable,%20long,%20long,%20java.util.concurrent.TimeUnit)
@@ -54,5 +54,16 @@ class MyToolWindow(private val toolWindow: ToolWindow) {
             return false
         }
         return true
+    }
+
+    private fun saveConfiguration(time: String, message: String) {
+        val file = File("D://timeNotificatorConfiguration.txt")
+        file.createNewFile() //creates new file if there was no such file
+        file.writeText(
+            "HOURS: " + time.substring(0, 2) + "\r\n" +
+             "MINUTES: " + time.substring(3, 5) + "\r\n" +
+             "MESSAGE: " + message
+        )
+
     }
 }
